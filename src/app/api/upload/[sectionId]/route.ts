cfafd4432b81e39cd41d1474f6944b5cf1183ec0
Rei <reicareich@gmail.com>
2025-10-28 16:03:51 -0500

    Add section and user check to file upload

--- src/app/api/upload/[sectionId]/route.ts
+++ src/app/api/upload/[sectionId]/route.ts
@@ -1,4 +1,9 @@
 import { NextResponse } from 'next/server';
+import { db } from '@src/server/db/index';
+import { file } from '@src/server/db/schema/file';
+import { section } from '@src/server/db/schema/section';
+import { eq } from 'drizzle-orm';
+import { getServerAuthSession } from '@src/server/auth';
 
 import fs from 'fs';
 import path from 'path';
@@ -9,40 +14,69 @@ export async function POST(
   req: Request,
   { params }: { params: { sectionId: string }}
 ) {
+  const session = await getServerAuthSession();
 
+  if(!session?.user?.id) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
+  // Local file upload
   const formData = await req.formData();
-  const file = formData.get('file') as File;
+  const newFile = formData.get('file') as File;
 
   const { sectionId } = await params;
 
-  if(!file || file.size === 0) {
+  let sectionName = await db.query.section.findFirst({
+    where: (sec) => eq(sec.id, sectionId)
+  })
+
+  if(!sectionName) {
+    return NextResponse.json(
+      { message: 'error', data: 'Section does not exist'},
+      { status: 400 }
+    )
+  }
+  
+  if(!newFile || newFile.size === 0) {
     return NextResponse.json(
       { message: 'error', data: 'No file attached' },
       { status: 400 }
     );
   }
 
-  if(!allowedTypes.includes(file.type)) {
+  if(!allowedTypes.includes(newFile.type)) {
     return NextResponse.json(
       { message: 'error', data: 'Must be png, jpeg, or pdf'},
       { status: 400 }
     )
   }
 
-  const buffer = Buffer.from(await file.arrayBuffer());
-
+  const buffer = Buffer.from(await newFile.arrayBuffer());
   const uploadDir = path.join(process.cwd(), 'public', 'uploads');
 
   if(!fs.existsSync(uploadDir)) {
     fs.mkdirSync(uploadDir, { recursive: true });
   }
 
-  const filePath = path.join(uploadDir, file.name);
-
+  const filePath = path.join(uploadDir, newFile.name);
   await fs.promises.writeFile(filePath, buffer);
 
+  const fileMetadata = {
+    id: "test_file_2",
+    authorId: session.user.id,
+    sectionId: sectionId,
+    file_name: newFile.name
+  }
+
+  try {
+    const result = await db.insert(file).values(fileMetadata).returning();
+  } catch (err) {
+    console.log(err);
+  }
+  
+
   return NextResponse.json(
-    { message: 'success', data: `File ${file.name} uploaded successfully` },
+    { message: 'success', data: `File ${newFile.name} uploaded successfully` },
     { status: 200 }
   )
 }
