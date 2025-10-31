ae88f8e930659b5d2a63f8734bf44102d51a8a38
vishyyyyyyyyy <217375979+vishyyyyyyyyy@users.noreply.github.com>
2025-10-30 21:34:45 -0500

    user profile page
    added a name, username, email, profile picture, post count, reports submitted, saved and uploaded files on the profile page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
 import { Bai_Jamjuree, Inter } from 'next/font/google';
 import { type Metadata } from 'next';
 import { GoogleAnalytics } from '@next/third-parties/google';
+import Link from 'next/link';
 
 import theme from '@src/utils/theme';
 
@@ -59,6 +60,11 @@ export default function RootLayout({
         {process.env.NEXT_PUBLIC_VERCEL_ENV === 'production' && (
           <GoogleAnalytics gaId="G-3NDS0P32CZ" />
         )}
+        <nav>
+          <Link href="/profile" className="px-3 py-1 hover:underline">
+            Profile
+          </Link>
+        </nav>
       </body>
     </html>
   );
