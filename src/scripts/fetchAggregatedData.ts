5fad4dcff197b262319634f8365422d610e9c7d5
amavankal <amavankal@gmail.com>
2025-11-29 00:25:49 -0600

    Fixed API key name

---
+++ src/scripts/fetchAggregatedData.ts
@@ -0,0 +1,31 @@
+/*
+Fetch the aggregated data from the API
+Taken from UTD Trends
+*/
+import { config } from 'dotenv';
+import { writeFileSync } from 'fs';
+import { resolve } from 'path';
+
+const envPath = resolve(__dirname, '../../.env.local');
+config({ path: envPath });
+
+const API_KEY = process.env.NEBULA_API_KEY;
+if (typeof API_KEY !== 'string') {
+  console.error('API key is undefined');
+} else {
+  const headers = {
+    'x-api-key': API_KEY,
+    Accept: 'application/json',
+  };
+
+  fetch('https://api.utdnebula.com/autocomplete/dag', {
+    method: 'GET',
+    headers: headers,
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      writeFileSync('src/data/aggregated_data.json', JSON.stringify(data));
+
+      console.log('Aggregated data fetched.');
+    });
+}
