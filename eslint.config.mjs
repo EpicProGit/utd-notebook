488419f7286151ecf4f6cda5975001040f5c126b
ShriramJana <shriram.j.iyengar@gmail.com>
2026-01-27 19:08:36 -0600

    Fix ESLint ESM import for eslint-config-next

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,4 @@
-import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextVitals from 'eslint-config-next/core-web-vitals.js';
 import nextTs from 'eslint-config-next/typescript';
 import prettier from 'eslint-config-prettier/flat';
 import { defineConfig, globalIgnores } from 'eslint/config';
