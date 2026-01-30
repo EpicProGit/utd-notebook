ad976bfbb10d6e59fe327ded0ec2557fe88b628a
ShriramJana <shriram.j.iyengar@gmail.com>
2026-01-30 16:19:02 -0600

    chore: revert config changes unrelated to homepage

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ import type { NextConfig } from 'next';
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
+(async () => {
+  await import('./src/env.mjs');
+})();
 
 /** @type {import("next").NextConfig} */
 const config: NextConfig = {
