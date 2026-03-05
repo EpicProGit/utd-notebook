7184165ad79cbac281c142d22f0077ece56d7cbb
EerajArRahman <realeeraj@gmail.com>
2026-03-04 18:56:13 -0600

    fix: escape apostrophe for eslint rule

--- src/components/form/CreatedNotes.tsx
+++ src/components/form/CreatedNotes.tsx
@@ -19,7 +19,7 @@ export default function CreatedNotes({ createdNotes }: CreatedNotesProps) {
         </div>
       ) : (
         <div className="w-full py-12 flex flex-col items-center justify-center border-2 border-dashed border-slate-200 dark:border-slate-800 rounded-xl text-md font-medium text-slate-600 dark:text-slate-400">
-          You haven't created any notes yet.
+        You haven&apos;t created any notes yet.
         </div>
       )}
     </Panel>
