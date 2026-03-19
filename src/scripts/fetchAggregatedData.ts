9c9dd2176ba548b4eb7fe9af4626a29abf3ac04c
Obinna Anaenugwu <oeanaenugwu@gmail.com>
2026-03-18 21:10:44 -0500

    api url change/fix

--- src/scripts/fetchAggregatedData.ts
+++ src/scripts/fetchAggregatedData.ts
@@ -17,7 +17,7 @@ if (typeof API_URL !== 'string') {
     Accept: 'application/json',
   };
 
-  await fetch('https://api.utdnebula.com/autocomplete/dag', {
+  await fetch(API_URL + '/autocomplete/dag', {
     method: 'GET',
     headers: headers,
   })
