5ec02d16e6b6840a23bf37c62dce928a41d82241
Maya M <mayam@Mayas-MacBook-Pro-3.local>
2026-04-01 11:30:13 -0500

    Update Settings subtitle to match utd clubs format(#143)

--- src/components/settings/SettingsHeader.tsx
+++ src/components/settings/SettingsHeader.tsx
@@ -17,7 +17,7 @@ export default function SettingsHeader({ user }: SettingsHeaderProps) {
               Settings
             </h1>
             <span className="text-xl max-sm:text-lg max-sm:text-center text-white opacity-80">
-              Manage your account preferences
+              Manage your account preferences and followed notebooks
             </span>
           </div>
           {user?.image && (
