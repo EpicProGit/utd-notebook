51bbbe06b5b3c5d7f13b9734eeb0a02bd111eaa3
Tyler Hill <gotyhill@gmail.com>
2026-04-01 18:34:26 -0500

    Format

--- src/app/notes/[id]/page.tsx
+++ src/app/notes/[id]/page.tsx
@@ -1,6 +1,6 @@
 import { notFound } from 'next/navigation';
-import { api } from '@src/trpc/server';
 import Header from '@src/components/header/Header';
+import { api } from '@src/trpc/server';
 
 type NotePageProps = {
   params: Promise<{ id: string }>;
@@ -19,19 +19,40 @@ export default async function NotePage({ params }: NotePageProps) {
         <p>{file.description || 'No provided description.'}</p>
 
         <section>
-            <p><strong>Course:</strong> {file.section?.prefix} {file.section?.number}</p>
-            <p><strong>Section: </strong> {file.section?.sectionCode}</p>
-            <p><strong>Professor:</strong> {file.section?.profFirst} {file.section?.profLast}</p>
-            <p><strong>Author:</strong> {file.author ? `${file.author.firstName} ${file.author.lastName}` : 'Unknown'}</p>
-            <p><strong>Last Updated:</strong> {file.updatedAt?.toLocaleDateString()}</p>
+          <p>
+            <strong>Course:</strong> {file.section?.prefix}{' '}
+            {file.section?.number}
+          </p>
+          <p>
+            <strong>Section: </strong> {file.section?.sectionCode}
+          </p>
+          <p>
+            <strong>Professor:</strong> {file.section?.profFirst}{' '}
+            {file.section?.profLast}
+          </p>
+          <p>
+            <strong>Author:</strong>{' '}
+            {file.author
+              ? `${file.author.firstName} ${file.author.lastName}`
+              : 'Unknown'}
+          </p>
+          <p>
+            <strong>Last Updated:</strong>{' '}
+            {file.updatedAt?.toLocaleDateString()}
+          </p>
         </section>
         <section>
-            <h2>PDF</h2>
-           {file.publicUrl ? (
-              <iframe src={file.publicUrl} title={file.name} width="100%" height="800px" />
-                ) : (
-              <p>PDF not available yet.</p>
-            )}
+          <h2>PDF</h2>
+          {file.publicUrl ? (
+            <iframe
+              src={file.publicUrl}
+              title={file.name}
+              width="100%"
+              height="800px"
+            />
+          ) : (
+            <p>PDF not available yet.</p>
+          )}
         </section>
       </main>
     </>
